115f1abdb08c5b58aea0be855aff394e1366fa03
Christopher Hoffmann <hoffdamann@gmail.com>
2015-04-07 21:02:20 -0600

    Text field/view clearing
    Added storyboard, set text field and textview and button as outlet properties, and create a clear method outlet for the button which clears the text in the text field and view.

--- DayX/DXAppDelegate.m
+++ DayX/DXAppDelegate.m
@@ -14,6 +14,9 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    self.window.rootViewController = [[UIStoryboard storyboardWithName:@"Storyboard" bundle:nil] instantiateInitialViewController];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
